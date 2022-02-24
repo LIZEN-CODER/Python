82952288f8b5c0437f198ecd4d2defbeefdab797
LIZEN-CODER <76940780+LIZEN-CODER@users.noreply.github.com>
2022-02-24 13:16:16 +0530

    Create Friendly_Console.py

---
+++ Friendly_Console.py
@@ -0,0 +1,8 @@
+# when you access any social networking website, you post comments, share information, and upload an image or video. 
+# All these activities leave your record on the web page.
+
+def my_statement():
+  statement = input("> Hi user !")
+  if statement == "hello":
+    print("> Hi ! I am the console, and how may I help you ? ")
+  
